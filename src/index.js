1cca121a81596fe88cc8aaa4655d573e0783ec84
Xin Ouyang <xin.ouyang@compass.com>
2021-07-09 19:28:57 +0800

    Feat: =

--- src/index.js
+++ src/index.js
@@ -1,56 +1,21 @@
 import * as parser from '@babel/parser';
 import traverse from '@babel/traverse';
 import generate from '@babel/generator';
-import t from '@babel/types';
-import camelcase from 'camelcase';
+import reactDirectives from 'babel-plugin-react-directives';
 
 const fileRegex = /\.(t|j)sx?$/;
-const flag = '___src2import_'
 
-let collectImports = [];
-
-
-export default function Src2Import() {
+export default function reactDirectivesPlugin() {
   return {
-    name: 'src2import',
+    name: 'react-directives',
 
     transform(code, path) {
       if (!fileRegex.test(path)) return code;
+      const ast = parser.parse(code, {sourceType: 'module', plugins: ['jsx', 'typescript']});
 
-      const ast = parser.parse(code, {sourceType: 'module'});
+      const {visitor} = reactDirectives({ version: '7.0.0' })
+      traverse(ast, visitor, undefined, {});
 
-
-      traverse(ast, {
-        ObjectProperty(path) {
-          if (!t.isIdentifier(path.node.key, {name: 'src'}) || !t.isStringLiteral(path.node.value)) {
-            return;
-          }
-
-          const {value} = path.node.value;
-          if (/^\w+:\/\//.test(value)) {
-            return;
-          }
-
-          const ident = t.identifier(flag + camelcase(value.replace(/\//g, ' ')));
-          if (!collectImports.some(item => item[1] === value)) {
-            collectImports.push([ident, value]);
-          }
-          path.node.value = ident;
-        },
-        Program: {
-          enter() {
-            collectImports = [];
-          },
-          exit(path) {
-            path.unshiftContainer(
-              'body',
-              collectImports.map(([ident, path]) => {
-                return t.importDeclaration([t.importDefaultSpecifier(ident)], t.stringLiteral(path));
-              }),
-            );
-          },
-        },
-      });
       const output = generate(ast);
       return output.code;
     },
